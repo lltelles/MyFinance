991b9ac31044e40cb141b8154e611e3ada856a09
Tiago Maro <tiago.machado3710@gmail.com>
2025-04-30 19:11:33 -0300

    Add: firebase update, gitignore Update: dashboard.html, TransactionForm.js, auth.js, Profile.js

--- js/components/dashboard/transactionForm/TransactionForm.js
+++ js/components/dashboard/transactionForm/TransactionForm.js
@@ -1,3 +1,6 @@
+import { db, auth } from "../../../app.js";
+import { collection, addDoc } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+
 class TransactionForm extends HTMLElement {
   constructor() {
     super()
